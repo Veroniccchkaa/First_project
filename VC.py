0e2c9506e207f70c7ff27bead929a62e63599e11
Veroniccchkaa <koreshkova.nika@icloud.com>
2024-08-08 03:48:57 +0300

    new list and dick

--- VC.py
+++ VC.py
@@ -1,2 +1,21 @@
-if __name__ == '__main__':
-	print ("Hello, world")
+a = [8, 4, 0, 34, 'qwerty', 'ad9']
+print(a)
+print(a[-1])
+print(a[0:2])
+print(a[:])
+a[4] = 987
+print(a[4])
+
+elements = {
+    'air' : 'воздух',
+    'water' : 'вода',
+    'earth' : 'земля',
+}
+elements['fire'] = 'огонь'
+print(elements)
+elementsValues = elements.values()
+print(elementsValues)
+elementsKeys = elements.keys()
+print(elementsKeys)
+len(elements)
+print(len(elements))
